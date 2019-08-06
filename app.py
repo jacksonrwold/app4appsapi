caa84bf120bcd0fe9cea7f7577b7fb6662ceeacd
jacksonrwold <jacksonrwold@gmail.com>
2019-08-06 14:52:32 -0700

    Added the delete and put parts

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app = Flask(__name__)
 CORS(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ammdwvexmwpbne:5f2956427a0fac13f1197aca445c0acc32ea8ff44f3b6e10d8b48d6d97be5a55@ec2-174-129-226-232.compute-1.amazonaws.com:5432/d8jaih58mshekl'
 
+# commands for remaking the database are "from app import db" and "db.create_all()"
 
 heroku = Heroku(app)
 db = SQLAlchemy(app)
@@ -28,7 +29,7 @@ class SubmissionData(db.Model):
 
 @app.route('/')
 def home():
-    return 'Hi from the App4Apps API'
+    return '<h1>Hi from the App4Apps API</h1>'
 
 @app.route('/submissionsdata/input', methods=['POST'])
 def dinosaur_input():
@@ -52,3 +53,22 @@ def return_submissiondata():
 def return_single_sumbissiondata(slug):
     single_submissiondata = db.session.query(SubmissionData.id, SubmissionData.name, SubmissionData.state, SubmissionData.description).filter(SubmissionData.name == slug).first()
     return jsonify(single_relic)
+
+@app.route('/submissionsdata/<id>', methods=['DELETE'])
+def delete_submissiondata(id):
+    if request.content_type == 'application/json':
+        record = db.session.query(SubmissionData).get(id)
+        db.session.delete(record)
+        db.session.commit()
+        return jsonify("Successfully deleted the suggestion")
+    return jsonify("Failed to delete the suggestion")
+
+@app.route('/submissionsdata/<id>', methods=['PUT'])
+def update_submissiondata(id):
+    if request.content_type == 'application/json':
+        name = put_data.get('name')
+        record = db.session.query(SubmissionData).get(id)
+        record.name = name
+        db.session.commit()
+        return jsonify("Successfully updated the suggestion")
+    return jsonify("Failed to update the suggestion")
